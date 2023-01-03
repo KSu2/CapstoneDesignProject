ddc9a9b63c415b87c51af08e2e04d81ed3f2d308
KSu2 <44729913+KSu2@users.noreply.github.com>
2023-01-03 00:44:29 -0500

    feat: added screen navigation

--- App/my-app/App.js
+++ App/my-app/App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from './components/LoginPage/LoginPage';
+import PicturePage from './components/PicturePage/PicturePage';
+import ForgotPage from './components/ForgotPage/ForgotPage';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>poop</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" component={LoginPage} />
+				<Stack.Screen name="PicturePage" component={PicturePage} />
+				<Stack.Screen name="ForgotPage" component={ForgotPage} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
