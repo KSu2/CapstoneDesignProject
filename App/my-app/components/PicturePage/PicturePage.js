322defa419719b5e18c9df5ef59b7bf2eb121232
KSu2 <44729913+KSu2@users.noreply.github.com>
2023-01-03 00:47:04 -0500

    feat: added PicturePage

---
+++ App/my-app/components/PicturePage/PicturePage.js
@@ -0,0 +1,19 @@
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	TextInput,
+	Button,
+	TouchableOpacity,
+} from 'react-native';
+
+const PicturePage = () => {
+	return (
+		<View>
+			<Text>PicturePage</Text>
+		</View>
+	);
+};
+
+export default PicturePage;
