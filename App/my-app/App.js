0889715845af1990c93c18328ca67c1e2cf7b63f
KSu2 <44729913+KSu2@users.noreply.github.com>
2023-01-03 18:50:18 -0500

    chore: added option for hiding screen header

--- App/my-app/App.js
+++ App/my-app/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginPage from './components/LoginPage/LoginPage';
-import PicturePage from './components/PicturePage/PicturePage';
-import ForgotPage from './components/ForgotPage/ForgotPage';
+import LoginScreen from './screens/LoginScreen';
+import PictureScreen from './screens/PictureScreen';
+import ForgotScreen from './screens/RegisterScreen';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -12,9 +12,21 @@ export default function App() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator initialRouteName="Home">
-				<Stack.Screen name="Home" component={LoginPage} />
-				<Stack.Screen name="PicturePage" component={PicturePage} />
-				<Stack.Screen name="ForgotPage" component={ForgotPage} />
+				<Stack.Screen
+					name="Home"
+					//options={{ headerShown: false }}
+					component={LoginScreen}
+				/>
+				<Stack.Screen
+					name="PicturePage"
+					//options={{ headerShown: false }}
+					component={PictureScreen}
+				/>
+				<Stack.Screen
+					name="ForgotPage"
+					//options={{ headerShown: false }}
+					component={ForgotScreen}
+				/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
