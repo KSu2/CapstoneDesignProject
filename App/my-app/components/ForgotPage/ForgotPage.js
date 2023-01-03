583abf280cf46e2652407fdd3c77b5f12fb9532f
KSu2 <44729913+KSu2@users.noreply.github.com>
2023-01-03 00:44:45 -0500

    feat: created ForgotPage

---
+++ App/my-app/components/ForgotPage/ForgotPage.js
@@ -0,0 +1,19 @@
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	TextInput,
+	Button,
+	TouchableOpacity,
+} from 'react-native';
+
+const ForgotPage = () => {
+	return (
+		<View>
+			<Text>ForgotPage</Text>
+		</View>
+	);
+};
+
+export default ForgotPage;
